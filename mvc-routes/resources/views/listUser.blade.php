eef563ef16f0f93317e28785952fdd806b453ad7
Otávio <otavio@houpa.app>
2020-12-04 09:02:17 -0300

    feat: mvc and route system

---
+++ mvc-routes/resources/views/listUser.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Tips</title>
+</head>
+<body>
+
+<h1>Listagem de usuário</h1>
+
+<p>
+    O nome do usuário é {{$user->name}} e o seu e-mail é {{$user->email}}
+</p>
+
+</body>
+</html>
