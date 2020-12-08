c6b3d1de67735b232d13152b729bb63e12bd21a1
Otávio <otavio@houpa.app>
2020-12-08 08:30:19 -0300

    feat: rename folders

--- ep08/app/Http/Controllers/AuthController.php
+++ ep08/app/Http/Controllers/AuthController.php
@@ -25,7 +25,7 @@ class AuthController extends Controller
     {
 
         if (!filter_var($request->email, FILTER_VALIDATE_EMAIL)) {
-            return redirect()->back()->withInput()->withErrors(['O e-mail informado não é válido!']);
+            return redirect()->back()->withInput()->withErrors(['O e-mail informado não é válido  !']);
         }
 
         $credentials = [
